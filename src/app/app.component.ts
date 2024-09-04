8bd050f7843d02ad28f26e2b7763ac4c39096136
Kevenson Leger <leger.kevenson@gmail.com>
2024-09-04 06:03:18 -0400

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
+import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
+import { TicketsComponent } from './dashboard/tickets/tickets.component';
+import { TrafficComponent } from './dashboard/traffic/traffic.component';
+import { HeaderComponent } from './header/header.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  imports: [
+    HeaderComponent,
+    ServerStatusComponent,
+    TrafficComponent,
+    TicketsComponent,
+    DashboardItemComponent,
+  ],
+})
+export class AppComponent {}
